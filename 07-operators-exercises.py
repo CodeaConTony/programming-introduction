3e38d1c397370f44dab2aeaebce38073abb8c1bb
Antony Huerto <codewithtony.yt@gmail.com>
2024-05-09 09:51:01 -0500

    fix

--- 07-operators-exercises.py
+++ 07-operators-exercises.py
@@ -4,28 +4,28 @@
 x = 7
 y = 10
 
-print(x > y)
+print(x > y) # False
 
-print(x < y)
+print(x < y) # True
 
-print(x >= y)
+print(x >= y) # False
 
-print(x <= y)
+print(x <= y) # True
 
-x += 2
+x += 2 # 9
 
-x -= 2
+x -= 2 # 7
 
-x *= 2
+x *= 2 # 14
 
-x /= 2
+x /= 2 # 7
 
-x //= 2
+x //= 2 # 3
 
-print(x)
+print(x) # 3
 
-print('a' < 'b' and 'c' > 'd')
+print('a' < 'b' and 'c' > 'd') # True - False -> False
 
-print('a' < 'b' and 'd' > 'c')
+print('a' < 'b' and 'd' > 'c') # True - True -> True
 
-print(x > y or x < y)
+print(x > y or x < y) # False - True -> True
