6b02d11452ffcedb2f08d24d92d44c971dee88dd
Antony Huerto <codewithtony.yt@gmail.com>
2024-05-09 09:38:51 -0500

    ADD: Exercises with operators

--- 06-booleans-and-operators.py
+++ 06-booleans-and-operators.py
@@ -6,64 +6,36 @@ es_administrador = False
 print(es_administrador)
 
 # Operadores de igualdad
-print(4 > 5)
-print(4 < 6)
-print(4 <= 5)
-print(4 >= 6)
-print(4 == 4)
-print(4 != 5)
+print(4 > 5) # False
+print(4 < 6) # True
+print(4 <= 5) # True
+print(4 >= 6) # False
+print(4 == 4) # True
+print(4 != 5) # True
 
 # Operadores logicos
-print(not 4 == 5)
-print(4 > 3 and 4 < 2)
-print(4 > 3 or 4 < 2)
+print(not 4 == 5)  # True
+print(4 > 3 and 4 < 2) # True - False -> False
+print(4 > 3 or 4 < 2) # True - False -> True
 
-# Operadores de asignación
+# # Operadores de asignación
 a = 4
 b = 5
 
 a += 4
 # a = a + 4
-print(a)
+print(a) # 8
 
 b -= 1
-print(b)
+# b = b - 1
+print(b) # 4
 
 a **= 2
-print(a)
+a = a ** 2
+print(a) # 64
 
-a *= 2
-a /= 4
-print(a)
-
-# Ejercicios con operadores
-
-# Intuye los resultados antes de ejecutar
-x = 7
-y = 10
-
-print(x > y)
-
-print(x < y)
-
-print(x >= y)
-
-print(x <= y)
-
-x += 2
-
-x -= 2
-
-x *= 2
-
-x /= 2
-
-x //= 2
-
-print(x)
-
-print('a' < 'b' and 'c' > 'd')
-
-print('a' < 'b' and 'd' > 'c')
-
-print(x > y or x < y)
+a *= 2 # 128
+# a = a *2
+a /= 4 # 32
+# a = a / 4
+print(a) # 32
---
+++ 07-operators-exercises.py
@@ -0,0 +1,31 @@
+# # Ejercicios con operadores
+
+# # Intuye los resultados antes de ejecutar
+x = 7
+y = 10
+
+print(x > y)
+
+print(x < y)
+
+print(x >= y)
+
+print(x <= y)
+
+x += 2
+
+x -= 2
+
+x *= 2
+
+x /= 2
+
+x //= 2
+
+print(x)
+
+print('a' < 'b' and 'c' > 'd')
+
+print('a' < 'b' and 'd' > 'c')
+
+print(x > y or x < y)
